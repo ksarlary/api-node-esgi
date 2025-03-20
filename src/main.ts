5bd0351d127da8adaf47e8d828a4a7d82aa128d4
Sonia2 <ksarlary@gmail.com>
2025-03-20 16:16:00 +0100

    Add DB

---
+++ src/main.ts
@@ -0,0 +1,24 @@
+import express from "express"
+import { initHandlers } from "./handlers/handler"
+import { AppDataSource } from "./db/database"
+
+const app = async () => {
+    const app = express()
+    const port = 3000
+    app.use(express.json())
+    initHandlers(app)
+
+    try{
+        await AppDataSource.initialize()
+    }catch(error){
+        if(error instanceof Error){
+            console.error(error.message)
+        }
+    }
+
+    app.listen(port, () => {
+        console.log(`Server running on http://localhost:${port}`)
+    })
+}
+
+app();
